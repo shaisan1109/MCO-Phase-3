8177a6c18277af5126a2a782030b180857904c31
unknown <shaisan1109@gmail.com>
2023-12-01 17:44:56 +0800

    Removed 'last active' feature

--- model/schema_user.js
+++ model/schema_user.js
@@ -16,10 +16,6 @@ const userSchema = new Schema({
 		type: String,
 		//required: true
 	},
-	lastActive: {
-		type: String,
-		//required: true
-	},
 	pronouns: {
 		type: String,
 		required: true
