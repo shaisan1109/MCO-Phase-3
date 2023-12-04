070b506a5e543d84ec038bf2a3a8ba4838bb87a3
LGGMaloles <134679747+LGGMaloles@users.noreply.github.com>
2023-12-04 16:53:13 +0800

    Add files via upload

---
+++ public/js/post.js
@@ -0,0 +1,50 @@
+const upvote = document.getElementById("upvote");
+const downvote = document.getElementById("downvote");
+
+upvote.addEventListener("click", async function(){
+    const title = document.getElementsByClassName('userpost-title');
+    const score = 1;
+    const post = {title: title, score: score}
+
+    console.log(post);
+    try {
+         const response = await fetch ('/post', {
+             method: 'PUT',
+             body: post,
+            headers: {
+             'Content-Type': 'application/json'
+            }
+         });
+        if (response.status == 200){
+             console.log('request sent succesfully');
+        } else {
+            console.error('An error has occurred in the server.');
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+downvote.addEventListener("click", async function(){
+    const title = document.getElementsByClassName('userpost-title');
+    const score = -1;
+    const post = {title: title, score: score}
+    
+    console.log(post);
+    try {
+         const response = await fetch ('/post', {
+             method: 'PUT',
+             body: post,
+            headers: {
+             'Content-Type': 'application/json'
+            }
+         });
+        if (response.status == 200){
+             console.log('request sent succesfully');
+        } else {
+            console.error('An error has occurred in the server.');
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
